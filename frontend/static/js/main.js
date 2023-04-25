7f6c60cdf92695d3683199efebd7627e26d2477b
shalluv <leon555thai@gmail.com>
2023-04-25 17:34:20 +0700

    feat: login logout

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -1,3 +1,25 @@
+const login = () => {
+  window.location.href = 'http://localhost:3000/courseville/auth_app';
+};
+
+const logout = () => {
+  window.location.href = `http://localhost:3000/courseville/logout`;
+};
+
+const getUserProfile = async () => {
+  const options = {
+    method: 'GET',
+    credentials: 'include',
+  };
+
+  await fetch('http://localhost:3000/courseville/get_profile_info', options)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById('username').innerHTML = data.user.firstname_en;
+    })
+    .catch((error) => console.error(error));
+};
+
 const toggleSidebar = () => {
   document.querySelector('.sidebar').classList.toggle('close');
 };
