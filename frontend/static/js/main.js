26fe2a7d34f1527d853d10927c5e2b2204aa2cca
shalluv <leon555thai@gmail.com>
2023-04-27 00:45:06 +0700

    feat: data fetching and lots of utils func

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -14,14 +14,124 @@ const getUserProfile = async () => {
 
   await fetch(`http://127.0.0.1:3000/courseville/get_profile_info`, options)
     .then((response) => response.json())
-    .then((data) => {
+    .then(async (data) => {
+      document.getElementById('login__button').classList.add('hide');
+      document.getElementById('reminder__holder').classList.remove('hide');
       document.getElementById('login__label').classList.add('hide');
       document.getElementById('username__label').classList.remove('hide');
       document.getElementById('logout__label').classList.remove('hide');
       document.getElementById('username__label').innerHTML =
         data.user.firstname_en;
+      await getProps();
     })
     .catch((error) => console.error(error));
+};
+
+const getProps = async () => {
+  const options = {
+    method: 'GET',
+    credentials: 'include',
+  };
+
+  await fetch(`http://127.0.0.1:3000/props`, options).then((response) =>
+    response.json().then((data) => {
+      data.assignments.forEach((assignment) => {
+        addAssignment(assignment);
+      });
+
+      data.courses.courses.forEach((course) => {
+        course.schedule.forEach((schedule) => {
+          addClass(course, schedule);
+        });
+      });
+    })
+  );
+};
+
+const addAssignment = (assignment) => {
+  const duetime = new Date(assignment.duetime * 1000);
+  const card = document.getElementById(
+    `${duetime.getFullYear()}-${duetime.getMonth()}-${duetime.getDate()}`
+  );
+  if (!card) return;
+
+  const cardEvent = document.createElement('a');
+  cardEvent.classList.add('card__event');
+  cardEvent.href = assignment.link;
+
+  const eventItem = document.createElement('div');
+  eventItem.classList.add('event__item');
+  eventItem.classList.add('assignment');
+
+  const eventDueTime = document.createElement('h4');
+  eventDueTime.classList.add('event__end__time');
+  eventDueTime.innerHTML = `${duetime.getHours()}:${duetime.getMinutes()}`;
+
+  const eventItemTitle = document.createElement('h5');
+  eventItemTitle.classList.add('event__item__title');
+  eventItemTitle.innerHTML = assignment.title;
+
+  const event__icon__holder = document.createElement('div');
+  event__icon__holder.classList.add('event__icon__holder');
+
+  const event__icon = document.createElement('img');
+  event__icon.classList.add('event__icon');
+  event__icon.src = './static/images/icon-assignment.svg';
+  event__icon.alt = 'icon for assignment event';
+
+  event__icon__holder.appendChild(event__icon);
+  eventItem.appendChild(eventItemTitle);
+  eventItem.appendChild(event__icon__holder);
+  cardEvent.appendChild(eventItem);
+  cardEvent.appendChild(eventDueTime);
+
+  card.appendChild(cardEvent);
+};
+
+const addClass = (course, schedule) => {
+  const startTime = new Date(schedule.start_time * 1000);
+  const card = document.getElementById(
+    `${startTime.getFullYear()}-${startTime.getMonth()}-${startTime.getDate()}`
+  );
+  if (!card) return;
+  const endTime = new Date(schedule.end_time * 1000);
+
+  const cardEvent = document.createElement('a');
+  cardEvent.classList.add('card__event');
+  cardEvent.href = course.link;
+
+  const eventStartTime = document.createElement('h4');
+  eventStartTime.classList.add('event__start__time');
+  eventStartTime.innerHTML = `${startTime.getHours()}:${startTime.getMinutes()}`;
+
+  const eventItem = document.createElement('div');
+  eventItem.classList.add('event__item');
+  eventItem.classList.add('onsite');
+
+  const eventEndTime = document.createElement('h4');
+  eventEndTime.classList.add('event__end__time');
+  eventEndTime.innerHTML = `${endTime.getHours()}:${endTime.getMinutes()}`;
+
+  const eventItemTitle = document.createElement('h5');
+  eventItemTitle.classList.add('event__item__title');
+  eventItemTitle.innerHTML = course.course_name;
+
+  const event__icon__holder = document.createElement('div');
+  event__icon__holder.classList.add('event__icon__holder');
+
+  const event__icon = document.createElement('img');
+  event__icon.classList.add('event__icon');
+  event__icon.src = './static/images/icon-onsite.svg';
+  event__icon.alt = 'icon for onsite event';
+
+  event__icon__holder.appendChild(event__icon);
+  eventItem.appendChild(eventItemTitle);
+  eventItem.appendChild(event__icon__holder);
+  cardEvent.appendChild(eventStartTime);
+  cardEvent.appendChild(eventItem);
+  cardEvent.appendChild(eventEndTime);
+
+  card.appendChild(cardEvent);
 };
 
 const toggleSidebar = () => {
@@ -32,6 +142,16 @@ const toggleReminderModal = () => {
   document.querySelector('.reminder__modal').classList.toggle('close');
 };
 
-const goToPreviousWeek = () => {};
+const goToPreviousWeek = () => {
+  const weekOffset = parseInt(window.location.href.split('/').pop()) || 0;
+  window.location.href = `/${weekOffset - 1 === 0 ? '' : weekOffset - 1}`;
+};
 
-const goToNextWeek = () => {};
+const goToNextWeek = () => {
+  const weekOffset = parseInt(window.location.href.split('/').pop()) || 0;
+  window.location.href = `/${weekOffset + 1 === 0 ? '' : weekOffset + 1}`;
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await getUserProfile();
+});
--- frontend/static/js/router.js
+++ frontend/static/js/router.js
@@ -1,32 +1,63 @@
 import Home from './views/Home.js';
 
+const pathToRegex = (path) =>
+  new RegExp('^' + path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)') + '$');
+
+const getParams = (match) => {
+  const values = match.result.slice(1);
+  const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(
+    (result) => result[1]
+  );
+
+  return Object.fromEntries(
+    keys.map((key, i) => {
+      return [key, values[i]];
+    })
+  );
+};
+
 const navigateTo = (url) => {
   history.pushState(null, null, url);
   router();
 };
 
 const router = async () => {
-  const routes = [{ path: '/', view: Home }];
+  const routes = [
+    {
+      path: '/:weekOffset',
+      view: Home,
+    },
+    { path: '/', view: Home },
+  ];
 
   const potentialMatches = routes.map((route) => {
     return {
       route: route,
-      isMatch: location.pathname === route.path,
+      result: location.pathname.match(pathToRegex(route.path)),
     };
   });
 
-  let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch);
+  let match = potentialMatches.find(
+    (potentialMatches) => potentialMatches.result !== null
+  );
 
   if (!match) {
     match = {
       route: routes[0],
-      isMatch: true,
+      result: [location.pathname],
     };
   }
-  const view = new match.route.view();
+
+  if (match.route.path === '/:weekOffset' && isNaN(match.result[1])) {
+    match.result[1] = 0;
+  }
+
+  const view = new match.route.view(getParams(match));
 
   document.querySelector('#app').innerHTML = await view.getHtml();
 };
+
+window.addEventListener('popstate', router);
 
 document.addEventListener('DOMContentLoaded', () => {
   document.body.addEventListener('click', (e) => {
